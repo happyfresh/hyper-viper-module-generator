7da3919d37f5e8f59f0bbd19030360a2c4a27a1e
Teguh Hidayatullah <JARVIS@Admins-MBP.localdomain>
2017-09-13 13:23:10 +0700

    Add ViewBehavior template on iOS View Module & iOS View Controller Module.

---
+++ Hyper/Templates/iOS View Controller Module/$HyperModuleNameViewBehavior.h
@@ -0,0 +1,16 @@
+//
+//  $HyperModuleNameViewBehavior.h
+//  $HyperAuthorOrganization
+//
+//  Created by $HyperAuthorName on $HyperCreatedDate.
+//  Copyright © $HyperCreatedYear $HyperAuthorOrganization Inc. All rights reserved.
+//
+
+#import "ViewBehavior.h"
+#import "$HyperModuleNameDisplayItem.h"
+
+@protocol $HyperModuleNameViewBehavior <ViewBehavior>
+
+- (void)showDisplayItem:($HyperModuleNameDisplayItem *)displayItem;
+
+@end
