09ae3fd56249cc42824bc4c07961bb78b71c9873
Teguh Hidayatullah <JARVIS@Admins-MBP.localdomain>
2017-09-13 13:57:49 +0700

    Add Display Item Template to iOS VC and iOS View Module

---
+++ Hyper/Templates/iOS View Controller Module/$HyperModuleNameDisplayItem.h
@@ -0,0 +1,13 @@
+//
+//  $HyperModuleNameDisplayItem.h
+//  $HyperAuthorOrganization
+//
+//  Created by $HyperAuthorName on $HyperCreatedDate.
+//  Copyright © $HyperCreatedYear $HyperAuthorOrganization Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface $HyperModuleNameDisplayItem : NSObject
+
+@end
---
+++ Hyper/Templates/iOS View Controller Module/$HyperModuleNameDisplayItem.m
@@ -0,0 +1,13 @@
+//
+//  $HyperModuleNameDisplayItem.m
+//  $HyperAuthorOrganization
+//
+//  Created by $HyperAuthorName on $HyperCreatedDate.
+//  Copyright © $HyperCreatedYear $HyperAuthorOrganization Inc. All rights reserved.
+//
+
+#import "$HyperModuleNameDisplayItem.h"
+
+@implementation $HyperModuleNameDisplayItem
+
+@end
